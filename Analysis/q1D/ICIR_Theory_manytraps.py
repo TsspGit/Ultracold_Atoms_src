87c50f8617c8131bc0367350714f79bb75f2c542
Tomy <tssanchezpastor@gmail.com>
2020-11-19 15:04:59 +0100

    Theory results

--- Analysis/q1D/ICIR_Theory_manytraps.py
+++ Analysis/q1D/ICIR_Theory_manytraps.py
@@ -3,22 +3,46 @@ __author__ = "@Tssp"
 __date__ = "18/11/20"
 import numpy as np
 from math import pi
+import sys
 import os
 from numba import njit, prange
 from utils.atomic_units import ao, vo, e, hbar, me, Eh, to
 from input_ICIR_Theory import *
+
+# Integral
+@njit(parallel=True)
+def trapezoidal_int(a, b, h, eps):
+    '''
+    Parameters:
+    -----------
+    a:   left limit of the X-axis
+    b:   right limit of the X-axis
+    h:   step size
+    eps: epsilon
+    
+    Outputs:
+    --------
+    Value of the integral applying the trapezoidal integration method: step * f(t)
+    '''
+    out = 0
+    N = int((2*b-2*a)/h)
+    t = np.arange(2*a, 2*b, h)
+    for j in prange(N):
+        out += np.sqrt(eta_x*eta_z) * np.exp(eps*t[j]/2) / np.sqrt((1 - np.exp(-t[j]))*(1 - np.exp(-eta_x*t[j]))*(1 - np.exp(-eta_z*t[j]))) - t[j]**(-3/2)
+    out *= h
+    return out
 
 # Parameters and Constants:
 mass  = m * 1.66053873e-27 / me # a.u
 wL    = wL * 1e-9 / ao # a.u
 ky    = 2*np.pi/wL
 # Intensities:
-Ix    = Ix * (1e4 / Eh * to * ao**2)
+Ix    = float(sys.argv[1]) * (1e4 / Eh * to * ao**2)
 Iy    = Iy * (1e4 / Eh * to * ao**2)
 Iz    = Iz * (1e4 / Eh * to * ao**2)
 # Potential depths:
-Vy    = alpha * Iy # a.u
 Vx    = alpha * Ix # a.u
+Vy    = alpha * Iy # a.u
 Vz    = alpha * Iz # a.u
 # Frequencies:
 wx    = np.sqrt( 2 * Vx * ky**2 / mass)       
@@ -52,10 +76,10 @@ print(f"""
            dho_z(a.u)     =    {dhoz}
            h              =    {h}
       """)
-
 # Energies needed for compute the integral:
 E_ICIR = wy*np.loadtxt(f'Results/ICIR_positions_{int(Ix/(1e4 / Eh * to * ao**2))}_{int(Iy/(1e4 / Eh * to * ao**2))}_{int(Iz/(1e4 / Eh * to * ao**2))}.txt')[2]
 Eo = (wx + wy + wz)/2
+En_CM = [1.5460962687803033e-10, 1.7540673119226125e-10] 
 print(f"""       
                   Energies
          -------------------------
@@ -95,28 +119,6 @@ print(f"""
             -(2,0,0):   {epsilon[1]}
 """)
 
-# Integral
-@njit(parallel=True)
-def trapezoidal_int(a, b, h, eps):
-    '''
-    Parameters:
-    -----------
-    a:   left limit of the X-axis
-    b:   right limit of the X-axis
-    h:   step size
-    eps: epsilon
-    
-    Outputs:
-    --------
-    Value of the integral applying the trapezoidal integration method: step * f(t)
-    '''
-    out = 0
-    N = int((2*b-2*a)/h)
-    t = np.arange(2*a, 2*b, h)
-    for j in prange(N):
-        out += np.sqrt(eta_x*eta_z) * np.exp(eps*t[j]/2) / np.sqrt((1 - np.exp(-t[j]))*(1 - np.exp(-eta_x*t[j]))*(1 - np.exp(-eta_z*t[j]))) - t[j]**(-3/2)
-    out *= h
-    return out
 # Create the partitions of the integral axis.
 a = h*1e-3
 b = h*1e6
@@ -133,7 +135,7 @@ for i in range(0, 7):
     ''')
     integral020 += trapezoidal_int(a, b, h*10**(-3+i), epsilon[0])
     integral200 += trapezoidal_int(a, b, h*10**(-3+i), epsilon[1])
-    print('\nDone!\n')
+    #print(f'integ{i+1}: ', trapezoidal_int(a, b, h*10**(-3+i), epsilon[1]))
     a = b
     b *= 10
 print(10*'-' + '\nBingo !' )
@@ -151,10 +153,10 @@ print(f"""
 
 if mode == 'C':
     np.savetxt(f'Results/ICIR_q1d_ix{int(Ix/(1e4 / Eh * to * ao**2))}_iy{int(Iy/(1e4 / Eh * to * ao**2))}_iz{int(Iz/(1e4 / Eh * to * ao**2))}_exact.dat', 
-      [wx/wy, a_ICIR020, a_ICIR200], header='wxwy, asc020, asc200')
+      [wx/wy, a_ICIR020, a_ICIR200], header='wxwy, a_ICIR020, a_ICIR200')
 elif mode == 'W':
     np.savetxt(f'Results/ICIR_q1d_ix{int(Ix/(1e4 / Eh * to * ao**2))}_iy{int(Iy/(1e4 / Eh * to * ao**2))}_iz{int(Iz/(1e4 / Eh * to * ao**2))}_C{C[0]}.dat', 
-      [wx/wy, a_ICIR020, a_ICIR200], header='wxwy, asc020, asc200')
+      [wx/wy, a_ICIR020, a_ICIR200], header='wxwy, a_ICIR020, a_ICIR200')
 
 
 
--- Analysis/q1D/input_ICIR_Theory.py
+++ Analysis/q1D/input_ICIR_Theory.py
@@ -1,15 +1,16 @@
 __author__ = "@Tssp"
 __date__ = "01/10/20"
 atom   = 'Li7'
-m      = 7.0160040            # mass in Da
-n      = 6                    # Expansion order
-Ix     = 7190                 # Laser intensity in mW/cm2 X
-Iy     = 4993                 # Laser intensity in mW/cm2 Y
-Iz     = 50                   # Laser intensity in mW/cm2 Z
-wL     = 1000                 # Wavelength in nm
-hbar   = 1                    # Planck reduced Constant
-alpha  = 200                  # Polarization a.u
-h      = 1e-6                 # Reference integration step
-mode   = 'C'                  # Compute mode: - C computes the constant of C*hbar*wz
-							  #               - W write your own C below
-C      = 0                    # Write value if mode=W
+m      = 7.0160040                                        # mass in Da
+n      = 6                                                # Expansion order
+Ix     = 7802                                             # Laser intensity in mW/cm2 X
+Iy     = 4993                                             # Laser intensity in mW/cm2 Y
+Iz     = 50                                               # Laser intensity in mW/cm2 Z
+wL     = 1000                                             # Wavelength in nm
+hbar   = 1                                                # Planck reduced Constant
+alpha  = 200                                              # Polarization a.u
+En_CM  = [1.5590550489957357e-10, 1.8190022319802564e-10] # (0,2,0) and (2,0.0) free energies
+h      = 1e-6                                             # Reference integration step
+mode   = 'W'                                              # Compute mode: - C computes the constant of C*hbar*wz
+							                              #               - W write your own C below
+C      = 2                                                # Write value if mode=W
