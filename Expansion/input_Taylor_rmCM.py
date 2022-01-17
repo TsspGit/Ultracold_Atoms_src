5de6612cac9eee6161891c294cdb25a9877db59b
Tomy <tssanchezpastor@gmail.com>
2022-01-17 11:49:40 +0100

    RbK coefficients

--- Expansion/input_Taylor_rmCM.py
+++ Expansion/input_Taylor_rmCM.py
@@ -3,30 +3,30 @@ __date__ = "14/10/20"
 
 # Atom 1:
 #########
-atom1       = 'Li7'             # Atom 1
-m1_value    = 12789.3927072494  # Li mass in a.u
+atom1       = 'Rb87'            # Atom 1
+m1_value    = 158425.72618      # Rb87 mass in a.u
 nx1         = 6                 # X-axis expansion order
 ny1         = 6                 # Y-axis expansion order
-nz1         = 6                # Z-axis expansion order
-Ix1         = 7802              # Laser intensity in mW/cm2
-Iy1         = 4993              # Laser intensity in mW/cm2
-Iz1         = 4993                # Laser intensity in mW/cm2
+nz1         = 6                 # Z-axis expansion order
+Ix1         = 394               # Laser intensity in mW/cm2
+Iy1         = 252               # Laser intensity in mW/cm2
+Iz1         = 252               # Laser intensity in mW/cm2
 wLx1        = 1000              # Wavelength in nm
 wLy1        = 1000              # Wavelength in nm
 wLz1        = 1000              # Wavelength in nm
-alpha1      = 200               # Polarizability
+alpha1      = 320               # Polarizability
 
 # Atom 2:
 #########
-atom2       = 'Li7'             # Atom 1
-m2_value    = 12789.3927072494  # Li mass in a.u
+atom2       = 'K41'             # Atom 1
+m2_value    = 74639.55078       # K40 mass in a.u
 nx2         = 6                 # X-axis expansion order
 ny2         = 6                 # Y-axis expansion order
 nz2         = 6                 # Z-axis expansion order
-Ix2         = 7802              # Laser intensity in mW/cm2
-Iy2         = 50              # Laser intensity in mW/cm2
-Iz2         = 50                # Laser intensity in mW/cm2
+Ix2         = 737               # Laser intensity in mW/cm2
+Iy2         = 6                 # Laser intensity in mW/cm2
+Iz2         = 6                 # Laser intensity in mW/cm2
 wLx2        = 1000              # Wavelength in nm
 wLy2        = 1000              # Wavelength in nm
 wLz2        = 1000              # Wavelength in nm
-alpha2      = 200               # Polarizability
+alpha2      = 290               # Polarizability
